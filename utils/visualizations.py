be7ba5a1ea5306015771cfbac817cfd8717f2dd8
Osman <org3700@gmail.com>
2025-12-24 00:40:03 +0400

    app 2.0

--- utils/visualizations.py
+++ utils/visualizations.py
@@ -2,7 +2,6 @@
 Visualization Utilities for Predictive Maintenance Dashboard
 Provides interactive and static plotting functions
 """
-
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
